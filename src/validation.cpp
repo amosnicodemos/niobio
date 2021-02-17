364e4175615b2e7ceb1ca312e01fa3dbc3f2e269
Marconi Soldate <soldate@gmail.com>
2021-02-17 10:50:15 -0300

    Subsidy

--- src/validation.cpp
+++ src/validation.cpp
@@ -1247,7 +1247,7 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     CAmount nSubsidy = 50 * COIN;
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
-    return nSubsidy;
+    return 0; // nSubsidy;
 }
 
 CoinsViews::CoinsViews(
