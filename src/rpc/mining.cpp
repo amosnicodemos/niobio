809063793239f7fcdce2987ae822080e3a8c5859
soldate <soldate@gmail.com>
2021-10-06 11:25:06 -0300

    mine before sync and cvs height

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -678,9 +678,9 @@ static RPCHelpMan getblocktemplate()
             throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, PACKAGE_NAME " is not connected!");
         }
 
-        if (active_chainstate.IsInitialBlockDownload()) {
-            throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, PACKAGE_NAME " is in initial sync and waiting for blocks...");
-        }
+        //if (active_chainstate.IsInitialBlockDownload()) {
+        //    throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, PACKAGE_NAME " is in initial sync and waiting for blocks...");
+        //}
     }
 
     static unsigned int nTransactionsUpdatedLast;
